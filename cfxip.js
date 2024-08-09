9e53033470eeaf9e0ab11e9073fe006689c5cbda
Lab <labservices68@gmail.com>
2024-08-09 14:42:37 +0200

    Update cfxip.js

--- cfxip.js
+++ cfxip.js
@@ -1,3 +1,11 @@
+/*
+Easy CFX.re Server IP Resolver
+
+Developed by real7lab
+
+discord.gg/visionn 
+*/
+
 const readline = require('readline');
 const axios = require('axios');
 const i = readline.createInterface({
@@ -30,7 +38,7 @@ function repeat() {
             console.log("\x1b[31mError: Unable to find the server! Verify the CFX.re address or check if the server is currently online.\x1b[0m\n");
         }
 
-        console.log("\x1bNote: If you have questions or issues you can add me on Discord 'real7lab'.\nAlso, ensure the server is operational!\x1b\n");
+        console.log("\x1bNote: If you have questions or issues you can add me on Discord 'real7lab'.\x1b\n");
 
         repeat(); 
     });
